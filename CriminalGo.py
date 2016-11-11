a0163c56dc9c4dbf25ead4a158c804a370399e34
Dhanesh <d_budhrani90@hotmail.com>
2016-11-11 12:27:26 +0100

    Pokemon loading

--- CriminalGo.py
+++ CriminalGo.py
@@ -1,9 +1,16 @@
+import csv
 
+from models import Pokemon
 
 def main():
-	print "main"
+	loadPokemonTypes()
 
-
+def loadPokemonTypes():
+	with open("datasets/pokemon.csv") as input:
+		reader = csv.reader(input, delimiter=';')
+		for row in reader:
+			pokemon = Pokemon(row[0], row[1], row[2])
+			print "ID = " + str(pokemon.id) + "; Name = " + pokemon.name + "; Rareness = " + str(pokemon.rareness)
 
 
 if __name__ == "__main__":
--- models/Pokemon.py
+++ models/Pokemon.py
@@ -3,6 +3,7 @@
 
 class Pokemon(object):
 
-	def __init__(self, _id, _rareness):
+	def __init__(self, _id, _name, _rareness):
 		self.id = _id
+		self.name = _name
 		self.rareness = _rareness
